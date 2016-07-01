22c66241d756ab9577cb16a2c1319dcb6f15980c
thekakester <mitcheld@mtu.edu>
2016-07-01 18:36:09 +0000

    Updated script for screen rotations

--- install_column.sh
+++ install_column.sh
@@ -27,48 +27,70 @@ fi
 # ENGLISH (US) LAYOUT #
 #######################
 echo -e "${DIV}Changing keyboard layout to ENGLISH (US)${DEF}"
+sleep 1
 sudo sed -i 's|XKBLAYOUT=....|XKBLAYOUT="en"|g' /etc/default/keyboard
 
 ################
 # Update repos #
 ################
 echo -e "${DIV}Updating repositories$DEF"
+sleep 1
 sudo apt-get update
+
+###########################
+# INSTALL LXDE (Optional) #
+###########################
+echo -e "${DIV}Installing (optional) desktop manager$DEF"
+sleep 1
+sudo apt-get install -y lxde
 
 ###############
 # INSTALL VIM #
 ###############
 echo -e "${DIV}Installing VIM${DEF}"
+sleep 1
 sudo apt-get install -y vim
 
 ###############
 # INSTALL GIT #
 ###############
 echo -e "${DIV}Installing GIT${DEF}"
+sleep 1
 sudo apt-get install -y git
 
 ################
 # INSTALL JAVA #
 ################
 echo -e "${DIV}Installing Java and tools$DEF"
+sleep 1
 sudo apt-get install -y oracle-java8-jdk
 
 ###############
 # INSTALL FBI #
 ###############
 echo -e "${DIV}Installing FBI$DEF"
+sleep 1
 sudo apt-get install -y fbi
+
+##################
+# INSTALL XINPUT #
+##################
+echo -e "${DIV}Installing XInput$DEF"
+sleep 1
+sudo apt-get install -y xinput
 
 ######################
 # INSTALL C Compiler #
 ######################
 echo -e "${DIV}Installing GCC and MAKE$DEF"
+sleep 1
 sudo apt-get install -y gcc g++ make
 
 #################
 # SPLASH SCREEN #
 #################
 echo -e "${DIV}Setting up splash screen$DEF"
+sleep 1
 sudo cp 00S0asplashscreen /etc/init.d/00S0asplashscreen
 sudo cp splash.png /etc/splash.png
 sudo chmod a+x /etc/init.d/00S0asplashscreen
@@ -78,6 +100,7 @@ sudo insserv /etc/init.d/00S0asplashscreen
 # REMOVE RAINBOW #
 ##################
 echo -e "${DIV}Removing startup rainbow splash screen$DEF"
+sleep 1
 sudo sed -i "s/disable_splash=./disable_splash=1/g" /boot/config.txt
 #If grep finds the string, don't execute the next part.
 #If not found, add it to the file
@@ -87,25 +110,42 @@ grep -q -F "disable_splash=1" /boot/config.txt || sudo echo -e "\ndisable_splash
 # HIDE RAS-PI LOGO #
 ####################
 echo -e "${DIV}Removing Raspberry Pi Logo on startup$DEF"
+sleep 1
 grep -q -F "logo.nologo" /boot/cmdline.txt || sudo sed -i "s/$/ logo.nologo/" /boot/cmdline.txt
 
 ############################
 # DISALLOW STARTUP LOGGING #
 ############################
 echo -e "${DIV}Disallowing programs to display output during startup$DEF"
+sleep 1
 grep -q -F "loglevel=1" /boot/cmdline.txt || sudo sed -i "s/$/ loglevel=1/" /boot/cmdline.txt
 
 ##########################
 # FORWARD CONSOLE OUTPUT #
 ##########################
 echo -e "${DIV}Sending startup text to a different terminal (tty3)$DEF"
+sleep 1
 sudo sed -i "s/console=tty1/console=tty3/g" /boot/cmdline.txt
 
 #################
 # ROTATE SCREEN #
 #################
 echo -e "${DIV}Rotating screen 270 degrees$DEF"
+sleep 1
 sudo sed -i "s/#*display_rotate=./display_rotate=3/g" /boot/config.txt
 grep -q -F "display_rotate=3" /boot/config.txt || echo -e "\ndisplay_rotate=3" >> /boot/config.txt
 
-echo -e "${DIV}Everything is done!   Please Reboot$DEF"
+#########################
+# CONFIGURE TOUCHSCREEN #
+#########################
+echo -e "{$DIV}Configuring touchscreen settings$DEF"
+grep -q -F "touchConfig.sh" /etc/init.d/rc.local || echo -e "\n/etc/init.d/touchConfig.sh" >> /etc/init.d/rc.local
+sudo cp touchConfig.sh /etc/init.d/touchConfig.sh
+sudo chmod a+x /etc/init.d/touchConfig.sh
+
+
+########
+# DONE #
+########
+echo -e "${DIV}Everything is done! Rebooting...$DEF"
+#sudo reboot
